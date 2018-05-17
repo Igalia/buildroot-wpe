821b59fdc47cbb2da2538737f23a5c7251999dc8
Adrian Perez de Castro <aperez@igalia.com>
2018-05-18 00:58:06 +0300

    wpebackend-fdo: bump to version 1.0
    This is the first non-prerelease version. The commit also adds
    checksums for the license.
    
    Signed-off-by: Adrian Perez de Castro <aperez@igalia.com>

--- package/wpebackend-fdo/wpebackend-fdo.mk
+++ package/wpebackend-fdo/wpebackend-fdo.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-WPEBACKEND_FDO_VERSION = 0.1-prerelease2
+WPEBACKEND_FDO_VERSION = 0.1
 WPEBACKEND_FDO_SITE = https://wpewebkit.org/releases
 WPEBACKEND_FDO_SOURCE = wpebackend-fdo-$(WPEBACKEND_VERSION).tar.xz
 WPEBACKEND_FDO_LICENSE = BSD-2-Clause
+WPEBACKEND_FDO_LICENSE_FILES = COPYING
 WPEBACKEND_FDO_INSTALL_STAGING = YES
 WPEBACKEND_FDO_DEPENDENCIES = libegl libglib2 libxkbcommon wayland wpebackend
 
