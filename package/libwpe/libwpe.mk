ba84514fd8676d98ecad376218e53e9cd5f11a6e
Munez <munez@localhost.localdomain>
2018-10-19 17:34:40 +0530

    Addresed review comments for pull request.

--- package/libwpe/libwpe.mk
+++ package/libwpe/libwpe.mk
@@ -10,8 +10,12 @@ LIBWPE_SOURCE = libwpe-$(LIBWPE_VERSION).tar.xz
 LIBWPE_LICENSE = BSD-2-Clause
 LIBWPE_INSTALL_STAGING = YES
 LIBWPE_DEPENDENCIES = libegl libxkbcommon
+
+# Workaround for https://github.com/raspberrypi/userland/issues/316
+ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
 LIBWPE_CONF_OPTS += \
-    -DCMAKE_C_FLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE" \
-    -DCMAKE_CXX_FLAGS="$(TARGET_CXXFLAGS) -D_GNU_SOURCE"
+	-DCMAKE_C_FLAGS='$(TARGET_CFLAGS) -D_GNU_SOURCE' \
+	-DCMAKE_CXX_FLAGS='$(TARGET_CFLAGS) -D_GNU_SOURCE'
+endif
 
 $(eval $(cmake-package))
